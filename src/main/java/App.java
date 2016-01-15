4043bfa43967feae78bbce220e108c0ab9b044c7
Chris Houghton and Illia Kuchko <ikuchko@gmail.com>
2016-01-14 16:52:23 -0800

    Add pages for creation contacts and numbers.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,51 @@ public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
 
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/blabla", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/contacts.vtl");
+
+      //Contact contact =
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/newContact", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/newContact.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/success", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/success.vtl");
+
+      Contact contact = new Contact(request.queryParams("firstName"), request.queryParams("lastName"), request.queryParams("dateOfBirth"));
+
+      model.put("contact", contact);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/newNumber", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/newNumber.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    post("/newNumber", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/success.vtl");
+      Phone phone = new Phone(Integer.parseInt(request.queryParams("areaCode")), (Integer.parseInt(request.queryParams("phoneNumber"))), request.queryParams("type"));
+      model.put("phone", phone);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
